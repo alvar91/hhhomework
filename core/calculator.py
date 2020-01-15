e9fb680f30dfdbed524c34ad76333a03ba03ffa6
Aleksey <aleksey91scorp@bk.ru>
2020-01-15 21:28:52 +0300

    all done

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,39 @@
-from decorators import cache_decorator
+#from decorators import cache_decorator
 
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
-
+    available = ["+", "-", "/", "*", "**"]
+    if operation not in available:
+        return "Неккоректная операция"
+    operations = {
+        "+": lambda x, y: x + y,
+        "-": lambda x, y: x - y,
+        "/": lambda x, y: x / y,
+        "*": lambda x, y: x * y,
+        "**": lambda x, y: x ** y
+    }
+    try:
+        result = operations[operation](a, b)
+    except ZeroDivisionError:
+        return "Ошибка! Деление на ноль"
+    else:
+        return result
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    while True:
+        try:
+            a = int(input('Введите число: '))
+            break
+        except ValueError:
+            print("Ошибка! Для ввода доступны только целые числа")
+    while True:
+        try:
+            b = int(input('Введите число: '))
+            break
+        except ValueError:
+            print("Ошибка! Для ввода доступны только целые числа")
+    operation = input('Введите операцию: ')
 
     print('Результат: ', calculator(a, b, operation))
--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,30 @@
 class Car:
     # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
     # Поля должны задаваться через конструктор
-    pass
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
 class Garage:
     # Написать класс гаража Garage, у которого есть поле списка машин
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+    def __init__(self, cars):
+        if type(cars) == type([]):
+            self.cars = cars
+        else:
+            print("Неверный тип данных")
+            exit
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def __getItem__(self, position):
+        return self.cars[position]
+
+    def __len__(self):
+        return len(self.cars)
+
+    def delete(self, index):
+        del self.cars[index]
